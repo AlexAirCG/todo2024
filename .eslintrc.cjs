6120c83dc908c62515ceef5b273988d0cb5a6472
AlexAirCG <alexairggg@yandex.ru>
2024-11-18 16:34:02 +0300

    18/november/16:32/change .eslintrc and next.config.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,14 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  root: true,
-  rules: {
-    "@typescript-eslint/no-require-imports": "off",
-  },
+  //   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  //   parser: "@typescript-eslint/parser",
+  //   plugins: ["@typescript-eslint"],
+  //   root: true,
+  //   rules: {
+  //     "@typescript-eslint/no-require-imports": "off",
+  //   },
+
+  extends: ["next", "prettier"],
+
+  extends: ["next/core-web-vitals", "next/typescript"],
 };
